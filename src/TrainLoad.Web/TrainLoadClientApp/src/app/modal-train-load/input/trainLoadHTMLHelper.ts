8d04b30090d5603188d1e23a25a85f848a079566
mch <michal.zeg@gmail.com>
2023-10-20 21:04:02 +0200

    fixed image of trains

--- src/TrainLoad.Web/TrainLoadClientApp/src/app/modal-train-load/input/trainLoadHTMLHelper.ts
+++ src/TrainLoad.Web/TrainLoadClientApp/src/app/modal-train-load/input/trainLoadHTMLHelper.ts
@@ -6,10 +6,6 @@ trainLoadTitleFactory[TrainLoadType.HSLMA] = 'Train Load - HSLM-A';
 trainLoadTitleFactory[TrainLoadType.HSLMB] = 'Train Load - HSLM-B';
 
 export const trainLoadImagePathFactory = {};
-trainLoadImagePathFactory[TrainLoadType.HSLMA] = environment.production
-    ? '/TrainLoadClientApp/dist/assets/HSLM-A.svg'
-    : './assets/HSLM-A.svg';
-trainLoadImagePathFactory[TrainLoadType.HSLMB] = environment.production
-    ? '/TrainLoadClientApp/dist/assets/HSLM-B.svg'
-    : './assets/HSLM-B.svg';
+trainLoadImagePathFactory[TrainLoadType.HSLMA] = './assets/HSLM-A.svg';
+trainLoadImagePathFactory[TrainLoadType.HSLMB] = './assets/HSLM-B.svg';
 
